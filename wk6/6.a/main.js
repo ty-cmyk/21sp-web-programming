841228fae44eba938b4bf4c439f69e1f0f4a49a0
Tyler Mann <74388126+ty-cmyk@users.noreply.github.com>
2021-02-28 16:39:02 -0500

    Awesome Llama Facts

---
+++ wk6/6.a/main.js
@@ -0,0 +1,31 @@
+// new instance of the Vue class
+var llamaFacts = new Vue({
+  el: "#llamaFacts",
+  data: {
+    state: 'default',
+    header: 'Llama Facts',
+    newFact: '',
+    facts: [
+        'Most herds of llamas are maintained by the Indians of Bolivia, Peru, Ecuador, Chile, and Argentina.',
+        'The llama is primarily a pack animal but is also used as a source of food, wool, hides, tallow for candles, and dried dung for fuel.',
+        'The largest of the lamoids, it averages 120 cm (47 inches) at the shoulder. A 113-kilogram (250-pound) llama can carry a load of 45–60 kg and average 25 to 30 km (15 to 20 miles) travel a day.',
+        'The llama is a gentle animal, but, when overloaded or maltreated, it will lie down, hiss, spit and kick, and refuse to move.'
+    ]
+    
+  },
+  methods: {
+      saveItem: function() {
+          this.facts.push(this.newFact);
+          this.newFact = '';
+      },
+      changeState: function(newState) {
+          this.state = newState;
+          this.newFact = '';
+      },
+      deleteItem: function() {
+          this.facts.pop(this.newFact);
+          this.newFact = '';
+      }
+
+  }
+});
